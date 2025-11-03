f7d9a30c7b13253060347f54297809b01513d9a1
775083111 <775083111@qq.com>
2025-11-03 17:19:03 +0800

    first commit

---
+++ core/device/tun/tun_wireguard_windows.go
@@ -0,0 +1,15 @@
+package tun
+
+import (
+	"golang.zx2c4.com/wireguard/tun"
+)
+
+const (
+	offset     = 0
+	defaultMTU = 0 /* auto */
+)
+
+func createTUN(name string, mtu int) (tun.Device, error) {
+	name = "FxProxy"
+	return tun.CreateTUN(name, mtu)
+}
